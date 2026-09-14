3c510dfb4a4ea7e682a535aac789487d51d27435
Franz Noel <franz@tanglao.org>
2026-09-14 10:47:42 -0700

    feat: Add album and photos for Feeding the Family

--- js/photo-gallery.js
+++ js/photo-gallery.js
@@ -11,16 +11,29 @@
     var title = dialog.querySelector("#photo-lightbox-title");
     var caption = dialog.querySelector(".photo-lightbox-caption p");
     var closeButton = dialog.querySelector(".photo-lightbox-close");
+    var previousButton = dialog.querySelector(".photo-lightbox-previous");
+    var nextButton = dialog.querySelector(".photo-lightbox-next");
+    var position = dialog.querySelector(".photo-lightbox-position");
+    var triggers = Array.prototype.slice.call(document.querySelectorAll(".photo-lightbox-trigger"));
     var activeTrigger = null;
+    var activeIndex = 0;
 
-    document.querySelectorAll(".photo-lightbox-trigger").forEach(function (trigger) {
+    function showPhoto(index) {
+        activeIndex = (index + triggers.length) % triggers.length;
+        activeTrigger = triggers[activeIndex];
+        image.src = activeTrigger.href;
+        image.alt = activeTrigger.querySelector("img").alt;
+        title.textContent = activeTrigger.dataset.photoTitle;
+        caption.textContent = activeTrigger.dataset.photoCaption;
+        position.textContent = "Photo " + (activeIndex + 1) + " of " + triggers.length;
+        previousButton.hidden = triggers.length < 2;
+        nextButton.hidden = triggers.length < 2;
+    }
+
+    triggers.forEach(function (trigger, index) {
         trigger.addEventListener("click", function (event) {
             event.preventDefault();
-            activeTrigger = trigger;
-            image.src = trigger.href;
-            image.alt = trigger.querySelector("img").alt;
-            title.textContent = trigger.dataset.photoTitle;
-            caption.textContent = trigger.dataset.photoCaption;
+            showPhoto(index);
             dialog.showModal();
             document.body.classList.add("lightbox-open");
             closeButton.focus();
@@ -32,6 +45,18 @@
     }
 
     closeButton.addEventListener("click", closeDialog);
+    previousButton.addEventListener("click", function () { showPhoto(activeIndex - 1); });
+    nextButton.addEventListener("click", function () { showPhoto(activeIndex + 1); });
+
+    dialog.addEventListener("keydown", function (event) {
+        if (event.key === "ArrowLeft") {
+            event.preventDefault();
+            showPhoto(activeIndex - 1);
+        } else if (event.key === "ArrowRight") {
+            event.preventDefault();
+            showPhoto(activeIndex + 1);
+        }
+    });
 
     dialog.addEventListener("click", function (event) {
         if (event.target === dialog) {
